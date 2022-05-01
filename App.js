af5e80c1743016cfa843a38bdfea68f1df13eae1
Cheol-Cheol <dream174822@gmail.com>
2022-05-02 00:10:20 +0900

    ✅ test

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Initailize project</Text>
+      <Text>Initailize project!</Text>
       <StatusBar style="auto" />
     </View>
   );
